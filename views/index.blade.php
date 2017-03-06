687de710f51b461b403bcb0e9ec3a19148920755
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-03-06 12:41:17 +0100

    Kleine aanpassing

--- views/index.blade.php
+++ views/index.blade.php
@@ -10,16 +10,15 @@
     @if(isset($birthday))
         @foreach ($birthday as $row)
 		<h1>{{$row['month']}}</h1>
-		<h2>{{$row['day']}}</h2>
         <p>
             <a href="edit.php?id={{$row['id']}}">
-                {{$row['person']}} {{$row['year']}}</a>
+                {{$row['person']}} ({{$row['day']}}/{{$row['month']}}/{{$row['year']}})</a>
                 
             <a href="delete.php?id={{$row['id']}}">x</a>
         </p>
     @endforeach
     @endif
-    
+
         <p><a href="create.php">+ Toevoegen</a></p>
 
 	</body>
