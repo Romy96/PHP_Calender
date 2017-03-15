4f92d750bc679feb35f8dc899bf3583ee5ad5d39
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-03-15 12:02:25 +0100

    Code aangepast om het $_POST probleem proberen op te lossen, maar niet gewerkt.

--- create_action.php
+++ create_action.php
@@ -3,27 +3,30 @@ require 'inc/connection.php';
 require 'inc/session.php';
 require 'inc/post.php';
 
-$Naam = $_POST['naam'];
-$Dag = $_POST['dag'];
-$Maand = $_POST['maand'];
-$Jaar = $_POST['jaar'];
+var_dump($_POST);
 
-if (empty($_POST['naam']) || empty($_POST['dag']) || empty($_POST['maand']) || empty($_POST['jaar'])) {
-	$_SESSION['errors'][] = 'Één of meerdere velden zijn niet ingevuld of er gaat iets niet goed.';
-	header('Location: create.php');
+if (empty($_POST)) {
+	$_SESSION['errors'][] = "Er is niks ingevuld of post is leeg!";
+	//header('location: create.php');
 	exit;
 }
 
-$sql = $db->prepare("INSERT INTO birthdays (person, day, month, year) VALUES (?, ?, ?, ?)");
-if ($sql->execute(array($Naam, $Dag, $Maand, $Jaar))) {
-	if ($sql->rowCount() == 0) {
-		$_SESSION['errors'][] = 'Het is niet gelukt om de gegevens in te voeren';
-		header('Location: create.php');
-		exit;
-	}
-	if ($sql->rowCount() == 1) {
-		$_SESSION['errors'][] = 'De gegevens van uw verjaardag zijn succesvol ingevoerd.';
-		header('Location: index.php');
-		exit;
-	}
+if (!empty($_POST)) {
+	$sql = $db->prepare("INSERT INTO birthdays (person, day, month, year) VALUES (:naam, :dag, :maand, :jaar)");
+
+	$sql->bindParam(':naam', $_POST['naam']);
+	$sql->bindParam(':dag', $_POST['dag']);
+	$sql->bindParam(':maand', $_POST['maand']);
+	$sql->bindParam(':jaar', $_POST['jaar']);
+
+	$Naam = $_POST['naam'];
+	$Dag = $_POST['dag'];
+	$Maand = $_POST['maand'];
+	$Jaar = $_POST['jaar'];
+
+	$sql->execute();
+
+	// Redirect to index.php
+	//header('Location: index.php');
+	exit;
 }
